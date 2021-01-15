ca0fc1b431de82a9c36f166811f5901407ada11c
Fede <chiellofederico@gmail.com>
2021-01-15 17:03:53 +0100

    for in

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// (1) Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso il for in tutte le proprietà.
+// (2) Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+// (3) Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+// Creare un oggetto con delle proprietà.
+var studente = {
+  nome : 'Federico',
+  cognome : 'Rossi',
+  eta : 26
+};
+// Stampare tutte le proprietà.
+for (var key in studente) {
+  console.log(studente[key]);
+}
